9d4bdad707ffe9719d297202cff86dcacdd74e34
Jacob Allen <jallen4991@gmail.com>
2015-09-26 22:35:12 -0400

    Declaring Vars

--- ftc_app-master/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MainOp4991.java
+++ ftc_app-master/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MainOp4991.java
@@ -1,1 +1,22 @@
 
+    //Controllers
+    DcMotorController.DeviceMode devMode;
+    DcMotorController driveController;
+    DcMotorController armController;
+    ServoController servoController;
+
+    //Drive Motors
+    DcMotor driveRight;
+    DcMotor driveLeft;
+
+    //Arm Motors
+    DcMotor armStage1;
+    DcMotor armStage2;
+
+    //Servo
+    Servo hookServo;
+    Servo triggerServo;
+
+    //Servo Position
+    double hookServoPosition;
+    double triggerServoPosition;
