d8ad64d0e69188e87af486eca145a3940029da55
Jacob Allen <jallen4991@gmail.com>
2015-09-26 22:38:41 -0400

    init the hardware

--- ftc_app-master/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MainOp4991.java
+++ ftc_app-master/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MainOp4991.java
@@ -20,3 +20,22 @@
     //Servo Position
     double hookServoPosition;
     double triggerServoPosition;
+    public void init() {
+
+        // Drive Motors
+        driveRight = hardwareMap.dcMotor.get("driveRight");
+        driveLeft = hardwareMap.dcMotor.get("driveLeft");
+
+        // Arm Motors
+        armStage1 = hardwareMap.dcMotor.get("armStage1");
+        armStage2 = hardwareMap.dcMotor.get("armStage2");
+
+        // Servos
+
+
+        // Controllers
+        driveController = hardwareMap.dcMotorController.get("driveController");
+        armController = hardwareMap.dcMotorController.get("armController");
+        servoController = hardwareMap.servoController.get("servoController");
+
+    }
